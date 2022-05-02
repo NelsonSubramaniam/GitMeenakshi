2bf1004e5b93a4f2940d864da31905e3148e2ff1
NelsonSubramaniam <nelsonbharathi.s@gmail.com>
2022-05-02 13:59:19 +0530

    first line created

---
+++ src/org/meenakshi/Meenakshi.java
@@ -0,0 +1,13 @@
+package org.meenakshi;
+
+public class Meenakshi {
+	
+	public static void main(String[] args) {
+		
+		String s= "Nelson";
+		
+		System.out.println("first line created");
+		
+	}
+
+}
